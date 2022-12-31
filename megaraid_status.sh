673ab02bbe3b182cbfe80fa4465762b051951da7
Stefan Pejcic <stefan@pejcic.rs>
2022-12-31 09:10:11 +0100

    Update megaraid_status.sh

--- megaraid_status.sh
+++ megaraid_status.sh
@@ -6,6 +6,9 @@ if ! [ -x "$(command -v megacli)" ]; then
   exit 1
 fi
 
+# Set the email address to send the notification to
+to=your@email.com
+
 # Parse command line options
 while getopts ":c" opt; do
   case $opt in
@@ -16,21 +19,44 @@ while getopts ":c" opt; do
   esac
 done
 
-# Run the MegaCLI command to get the status of the RAID array
-status=$(megacli -LDInfo -Lall -aALL | grep 'State' | awk '{print $3}')
+# Get the current status of the RAID array
+current_status=$(megacli -LDInfo -Lall -aALL | grep 'State' | awk '{print $3}')
+
+# Get the current number of disks in the RAID array
+current_disks=$(megacli -PDList -aALL | grep 'Device Id' | wc -l)
+
+# Check if the current status or number of disks is different from the previous values
+if [ -f "/tmp/megaraid_status" ]; then
+  previous_status=$(cat /tmp/megaraid_status)
+  previous_disks=$(cat /tmp/megaraid_disks)
+  if [ "$current_status" != "$previous_status" ] || [ "$current_disks" != "$previous_disks" ]; then
+    # Send an email notification
+    message="MegaRAID status has changed:"
+    if [ "$current_status" != "$previous_status" ]; then
+      message="$message\n  Status: $previous_status -> $current_status"
+    fi
+    if [ "$current_disks" != "$previous_disks" ]; then
+      message="$message\n  Number of disks: $previous_disks -> $current_disks"
+    fi
+    echo -e "$message" | mail -s "MegaRAID status change" $to
+  fi
+fi
+
+# Save the current status and number of disks to temporary files
+echo "$current_status" > /tmp/megaraid_status
+echo "$current_disks" > /tmp/megaraid_disks
 
 # Check if the count option was specified
 if [ "$count" = true ]; then
-  # Get the number of disks in the RAID array
-  disks=$(megacli -PDList -aALL | grep 'Device Id' | wc -l)
-  echo "The MegaRAID array has $disks disks."
+  # Print the number of disks in the RAID array
+  echo "The MegaRAID array has $current_disks disks."
 else
-  # Check the status and print the appropriate message
-  if [ "$status" == "Optimal" ]; then
+  # Print the status of the MegaRAID array
+  if [ "$current_status" == "Optimal" ]; then
     echo "The MegaRAID array is in optimal condition."
-  elif [ "$status" == "Degraded" ]; then
+  elif [ "$current_status" == "Degraded" ]; then
     echo "The MegaRAID array is degraded."
-  elif [ "$status" == "Failed" ]; then
+  elif [ "$current_status" == "Failed" ]; then
     echo "The MegaRAID array has failed."
   else
     echo "The MegaRAID array status is unknown."
