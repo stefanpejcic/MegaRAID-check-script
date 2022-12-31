440a079393995c805b401db69a60ce9f0f71ce45
Stefan Pejcic <stefan@pejcic.rs>
2022-12-31 09:07:25 +0100

    Update megaraid_status.sh

--- megaraid_status.sh
+++ megaraid_status.sh
@@ -6,16 +6,33 @@ if ! [ -x "$(command -v megacli)" ]; then
   exit 1
 fi
 
+# Parse command line options
+while getopts ":c" opt; do
+  case $opt in
+    c) count=true ;;
+    \?) echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+  esac
+done
+
 # Run the MegaCLI command to get the status of the RAID array
 status=$(megacli -LDInfo -Lall -aALL | grep 'State' | awk '{print $3}')
 
-# Check the status and print the appropriate message
-if [ "$status" == "Optimal" ]; then
-  echo "The MegaRAID array is in optimal condition."
-elif [ "$status" == "Degraded" ]; then
-  echo "The MegaRAID array is degraded."
-elif [ "$status" == "Failed" ]; then
-  echo "The MegaRAID array has failed."
+# Check if the count option was specified
+if [ "$count" = true ]; then
+  # Get the number of disks in the RAID array
+  disks=$(megacli -PDList -aALL | grep 'Device Id' | wc -l)
+  echo "The MegaRAID array has $disks disks."
 else
-  echo "The MegaRAID array status is unknown."
+  # Check the status and print the appropriate message
+  if [ "$status" == "Optimal" ]; then
+    echo "The MegaRAID array is in optimal condition."
+  elif [ "$status" == "Degraded" ]; then
+    echo "The MegaRAID array is degraded."
+  elif [ "$status" == "Failed" ]; then
+    echo "The MegaRAID array has failed."
+  else
+    echo "The MegaRAID array status is unknown."
+  fi
 fi
